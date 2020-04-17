433c63053dd98c6a6068eeaddbdab3504d2822bf
Ianice Lanziloti <ianice_lanziloti@hotmail.com>
2020-04-17 03:38:41 -0300

    Implatando logica basica

--- src/Components/Display/Display.jsx
+++ src/Components/Display/Display.jsx
@@ -1,3 +1,8 @@
 import React from 'react';
 import './Display.css';
-export default (props) => <div className='display'>{props.value}</div>;
+export default (props) => (
+  <div className='display'>
+    <div className='history'>{props.history}</div>
+    <div className='value'>{props.value}</div>
+  </div>
+);
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,47 +1,91 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import './Calculator.css';
 
 import Button from '../Components/Button/Button';
 import Display from '../Components/Display/Display';
 
-export default class Calculator extends Component {
-  clearMemory() {
-    console.log('limpar');
-  }
+export default (props) => {
+  const [value, setValue] = useState('0');
+  const [memory, setMemory] = useState(null);
+  const [history, setHistory] = useState('');
+  const [display, setDisplay] = useState('0');
+  const [operation, setOperation] = useState('');
+  const [clear, setClear] = useState(false);
 
-  setOperation(operation) {
-    console.log(operation);
-  }
+  const clearMemory = () => {
+    setMemory(null);
+    setOperation('');
+    setValue('0');
+    setHistory('');
+    setClear(false);
+  };
 
-  addDigit(n) {
-    console.log(n);
-  }
+  const calc = (op) => {
+    switch (op) {
+      case '+':
+        setMemory(memory + parseFloat(value));
+        break;
+      case '-':
+        setMemory(memory - parseFloat(value));
+        break;
+      case '/':
+        if (value !== '0') setMemory(memory / parseFloat(value));
+        break;
+      case '*':
+        setMemory(memory * parseFloat(value));
+        break;
+      default:
+        break;
+    }
+    setHistory(`${memory} ${operation} ${value}`);
+  };
 
-  render() {
-    const addDigit = (n) => this.addDigit(n);
-    const setOperation = (op) => this.setOperation(op);
+  const addOperation = (newOp) => {
+    if (!value) return;
 
-    return (
-      <div className='calculator'>
-        <Display value={0} />
-        <Button label='AC' click={() => this.clearMemory()} triple />
-        <Button label='/' click={setOperation} operation />
-        <Button label='7' click={addDigit} />
-        <Button label='8' click={addDigit} />
-        <Button label='9' click={addDigit} />
-        <Button label='*' click={setOperation} operation />
-        <Button label='4' click={addDigit} />
-        <Button label='5' click={addDigit} />
-        <Button label='6' click={addDigit} />
-        <Button label='-' click={setOperation} operation />
-        <Button label='1' click={addDigit} />
-        <Button label='2' click={addDigit} />
-        <Button label='3' click={addDigit} />
-        <Button label='+' click={setOperation} operation />
-        <Button label='0' click={addDigit} double />
-        <Button label='.' click={setOperation} />
-        <Button label='=' click={setOperation} operation />
-      </div>
-    );
-  }
-}
+    if (memory !== null) {
+      if (operation && (operation === newOp || newOp === '=' || !clear)) calc(operation);
+    } else {
+      setMemory(parseFloat(value));
+    }
+    if (newOp !== '=') setOperation(newOp);
+    setClear(true);
+  };
+
+  const addDigit = (dig) => {
+    if (dig === '.' && value.includes('.')) return;
+
+    if (value === '0' || clear) {
+      setValue(dig);
+      setClear(false);
+    } else setValue(value + dig);
+  };
+
+  useEffect(() => {
+    setDisplay(clear ? memory : value || '0');
+  }, [memory, clear, value]);
+
+  return (
+    <div className='calculator'>
+      <Display history={history} value={display} />
+      <Button label='AC' click={() => clearMemory()} triple />
+      <Button label='/' click={addOperation} operation />
+      <Button label='7' click={addDigit} />
+      <Button label='8' click={addDigit} />
+      <Button label='9' click={addDigit} />
+      <Button label='*' click={addOperation} operation />
+      <Button label='4' click={addDigit} />
+      <Button label='5' click={addDigit} />
+      <Button label='6' click={addDigit} />
+      <Button label='-' click={addOperation} operation />
+      <Button label='1' click={addDigit} />
+      <Button label='2' click={addDigit} />
+      <Button label='3' click={addDigit} />
+      <Button label='+' click={addOperation} operation />
+      <Button label='0' click={addDigit} double />
+      <Button label='.' click={addDigit} />
+      <Button label='=' click={addOperation} operation />
+    </div>
+  );
+};
